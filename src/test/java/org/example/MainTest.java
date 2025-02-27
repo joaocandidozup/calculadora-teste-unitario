af6a85705c5ae4e70252cf6e71d848651b56c613
joaocandidozup <joao.candido@zup.com>
2025-02-27 10:19:17 -0300

    test: add unit test to validate failure  scenarios in the subtraction operation in the calculate method

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -49,5 +49,19 @@ public class MainTest {
         // Assert
         assertEquals(resultadoEsperado, resultado);
     }
+    @Test
+    public void testSubtracaoFail() {
+        // Arrange
+        double numero1 = 10;
+        double numero2 = 5;
+        int operacao = 2;
+        String resultadoNaoEsperado = "O resultado de 10,0 - 5,0 = 3,0";
+
+        // Act
+        String resultado = Main.calcular(numero1, numero2, operacao);
+
+        // Assert
+        assertNotEquals(resultadoNaoEsperado, resultado);
+    }
 
 }
