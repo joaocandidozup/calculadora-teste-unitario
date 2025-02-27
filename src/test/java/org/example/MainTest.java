2dcfef21bc4ad65f0bf051fe18fad67dbf5f6557
joaocandidozup <joao.candido@zup.com>
2025-02-27 09:53:56 -0300

    test: adds unit test to validate failure scenarios in the sum operation in the calculate method

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MainTest {
 
@@ -19,5 +20,19 @@ public class MainTest {
 
         // Assert
         assertEquals(resultadoEsperado, resultado);
+    }
+    @Test
+    public void testSomaFail() {
+        // Arrange
+        double numero1 = 10;
+        double numero2 = 5;
+        int operacao = 1;
+        String resultadoNaoEsperado = "O resultado de 10,0 + 5,0 = 20,0";
+
+        // Act
+        String resultado = Main.calcular(numero1, numero2, operacao);
+
+        // Assert
+        assertNotEquals(resultadoNaoEsperado, resultado);
     }
 }
