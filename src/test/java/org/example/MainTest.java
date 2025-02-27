ccc54e47b1c0564fd1a2eadb4468519c2c2e9d5b
joaocandidozup <joao.candido@zup.com>
2025-02-27 09:41:55 -0300

    test: add unit test for the calculate method

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,5 +1,23 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
 
+    @Test
+    public void testSomaSucess() {
+        // Arrange
+        double numero1 = 10;
+        double numero2 = 5;
+        int operacao = 1;
+        String resultadoEsperado = "O resultado de 10,0 + 5,0 = 15,0";
+
+        // Act
+        String resultado = Main.calcular(numero1, numero2, operacao);
+
+        // Assert
+        assertEquals(resultadoEsperado, resultado);
+    }
 }
